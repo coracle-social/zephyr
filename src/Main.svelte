f942669a2c9e670c97ef6114f09e2b4e8c1fa112
Jonathan Staab <shtaab@gmail.com>
2023-10-09 09:03:00 -0700

    Add mobile support with swipe to navigate #1

--- src/Main.svelte
+++ src/Main.svelte
@@ -35,11 +35,53 @@
     arrowsVisible = true
   }
 
+  let touch
+  let touchX
+  let touchT
+
+  const onTouchStart = e => {
+    touch = e.touches[0]
+    touchX = touch.clientX
+    touchT = Date.now()
+
+    card.classList.remove('transition-all')
+  }
+
+  const onTouchMove = e => {
+    touch = e.touches[0]
+
+    card.style = `left: ${touch.clientX - touchX}px`
+  }
+
+  const onTouchEnd = e => {
+    const offset = touch.clientX - touchX
+    const timing = Date.now() - touchT
+    const velocity = Math.abs(offset / timing)
+    const showPrev = velocity > 1 && offset > 0 && $cursor > 0
+    const showNext = velocity > 1 && offset < 0
+
+    if (showPrev) {
+      card.style = `left: 500px`
+      prev()
+    } else if (showNext) {
+      card.style = `left: -500px`
+      next()
+    } else {
+      card.classList.add('transition-all')
+      card.style = `left: 0px`
+    }
+  }
+
+  let card
   let authors
   let arrowsVisible = true
 
   onMount(async () => {
     loadPage()
+
+    document.addEventListener('touchstart', onTouchStart)
+    document.addEventListener('touchmove', onTouchMove)
+    document.addEventListener('touchend', onTouchEnd)
 
     window.addEventListener('keydown', e => {
       if (arrowsVisible && e.key === 'ArrowRight') next()
@@ -49,19 +91,19 @@
 </script>
 
 {#if $event}
-  <div transition:fade class="flex max-h-full w-full">
+  <div transition:fade bind:this={card} class="flex max-h-full w-full absolute" style="left: 0px">
     <div
-      class="p-12 text-gray-700 hover:text-gray-100 transition-all duration-[2000ms] flex-grow flex items-center"
+      class="p-12 text-gray-700 hover:text-gray-100 transition-all duration-[2000ms] flex-grow flex items-center hidden sm:block"
       class:cursor-pointer={arrowsVisible}
       class:opacity-0={!arrowsVisible}
       on:click={arrowsVisible ? prev : null}>
       <i class="fa fa-chevron-left" />
     </div>
-    <div class="py-8 flex overflow-hidden">
+    <div class="py-8 flex overflow-hidden mx-4 sm:m-0">
       <EventCard event={$event} />
     </div>
     <div
-      class="p-12 text-gray-700 hover:text-gray-100 transition-all duration-[2000ms] flex-grow flex items-center"
+      class="p-12 text-gray-700 hover:text-gray-100 transition-all duration-[2000ms] flex-grow flex items-center hidden sm:block"
       class:cursor-pointer={arrowsVisible}
       class:opacity-0={!arrowsVisible}
       on:click={arrowsVisible ? next : null}>
